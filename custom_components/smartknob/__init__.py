f7926c28e469528e20606e65ce3595a2aa83727e
carlhampuswall <carl.hampus.wall@gmail.com>
2023-11-15 22:08:00 +0000

    Commit to dev branch

--- custom_components/smartknob/__init__.py
+++ custom_components/smartknob/__init__.py
@@ -1,7 +1,13 @@
 import logging
 import json
 
+from .apps import Apps
+
+from homeassistant.core import HomeAssistant, Config
 from homeassistant.const import STATE_ON, STATE_OFF, SERVICE_TURN_ON, SERVICE_TURN_OFF
+from .dimmercontroller import DimmerController
+from .switchcontroller import SwitchController
+from homeassistant.helpers.entity_registry import async_validate_entity_id, async_get
 
 from .const import (
     DOMAIN,
@@ -14,13 +20,22 @@ from .const import (
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config):
     _LOGGER.debug("async_setup")
-    # hass.async_create_task(hass.config_entries.flow.async_init(DOMAIN))
+
+    apps = Apps()
+
+    apps.add(DimmerController(hass, "light.virtual_light_1"))
+    apps.add(SwitchController(hass, "switch.virtual_switch_1"))
+
+    async def init_knob():
+        await hass.components.mqtt.async_publish(
+            hass,
+            TOPIC_TO_KNOB,
+            json.dumps(await apps.async_get_all_for_knob()),
+        )
 
     async def mqtt_message_recived(msg):
-        # _LOGGER.error("Message recived on topic %s: %s", msg.topic, msg.payload)
-
         try:
             payload = json.loads(msg.payload)
         except json.JSONDecodeError as e:
@@ -38,44 +53,22 @@ async def async_setup(hass, config):
         entity_id = payload["entity_id"]
         app_slug = payload["app_slug"]
         new_value = payload["new_value"]
-        # data = payload["data"]
-
-        # _LOGGER.error(payload)
 
         if app_slug == LIGHT_SWITCH:
             _LOGGER.error("Switch command executing")
-            if new_value == 1.0:
-                await hass.services.async_call(
-                    "light", "turn_on", {"entity_id": entity_id}
-                )
-
-            elif new_value == 0.0:
-                await hass.services.async_call(
-                    "light", "turn_off", {"entity_id": entity_id}
-                )
-
-            else:
-                _LOGGER.error("Not implemented command")
+            await apps.get(entity_id).async_set_value(new_value)
 
         elif app_slug == LIGHT_DIMMER:
             _LOGGER.error("Light command executing")
-            if new_value != None:
-                await hass.services.async_call(
-                    "light",
-                    SERVICE_TURN_ON if new_value > 0.0 else SERVICE_TURN_OFF,
-                    {"entity_id": entity_id, "brightness": new_value * 2.55}
-                    if new_value > 0.0
-                    else {"entity_id": entity_id},
-                )
-                # _LOGGER.error(hass.states.get(entity_id))
-            else:
-                _LOGGER.error("Not implemented command")
+            await apps.get(entity_id).async_set_value(new_value)
 
         else:
-            _LOGGER.error("No implemented app_id")
+            _LOGGER.error("No implemented app_slug")
             return
 
     await hass.components.mqtt.async_subscribe(TOPIC_TO_HASS, mqtt_message_recived, 0)
+
+    await init_knob()
 
     return True
 
