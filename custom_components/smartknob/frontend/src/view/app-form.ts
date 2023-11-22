780160b017353000a1def2d92234c95d78751cae
carlhampuswall <carl.hampus.wall@gmail.com>
2023-11-22 23:27:06 +0000

    UI changes

--- custom_components/smartknob/frontend/src/view/app-form.ts
+++ custom_components/smartknob/frontend/src/view/app-form.ts
@@ -9,8 +9,9 @@ import {
 } from '../types';
 import { HassEntity } from 'home-assistant-js-websocket';
 import '../components/SkReorderableList';
-import { mdiPlus } from '@mdi/js';
+import { mdiPlus, mdiSort } from '@mdi/js';
 import { saveApp } from '../data/websockets';
+// import { selectSelector } from '../const';
 
 @customElement('app-form')
 export class AppForm extends LitElement {
@@ -19,22 +20,34 @@ export class AppForm extends LitElement {
       display: block;
       max-width: 600px;
     }
+    ha-selector {
+      min-width: 200px;
+      width: 100%;
+    }
+
     .add-app {
+      width: 100%;
       display: flex;
       gap: 12px;
       padding-bottom: 12px;
+      align-items: center;
+    }
+
+    .btn {
+      height: 100%;
+      aspect-ratio: 1/1;
     }
   `;
 
   @property({ type: Object }) hass!: HomeAssistant;
   @property({ type: Array }) entities!: HassEntity[];
   @property({ type: Array }) appSlugs!: AppSlug[];
-  @property({ type: Array }) appList!: AppListItem[];
+  @property({ type: Array }) apps!: AppListItem[];
 
   @state() private _selectedSlug: AppSlug | null = null;
   @state() private _selectedEntity: HassEntity | null = null;
-  // @state() private _appList: AppListItem[] = [];
   @state() private _domain: string = '';
+  @state() private _sortable: boolean = false;
 
   connectedCallback(): void {
     super.connectedCallback();
@@ -45,8 +58,8 @@ export class AppForm extends LitElement {
   render() {
     const options: SelectOption[] = this.appSlugs.map((slug) => {
       return {
-        value: slug.slug,
-        label: slug.friendlyName,
+        value: slug.slug_id,
+        label: slug.friendly_name,
       };
     });
 
@@ -64,8 +77,8 @@ export class AppForm extends LitElement {
           .hass=${this.hass}
           .selector=${selectSelector}
           .required=${true}
-          .label=${'Select App Slug'}
-          .value=${this._selectedSlug?.slug}
+          .label=${'Select App'}
+          .value=${this._selectedSlug?.slug_id}
           @value-changed=${this.appSlugChanged}
         ></ha-selector>
         <ha-selector
@@ -73,7 +86,17 @@ export class AppForm extends LitElement {
           .selector=${{
             entity: {
               include_entities: this.entities.map((entity) => {
-                if (!entity.entity_id.startsWith(this._domain)) {
+                if (
+                  !entity.entity_id.startsWith(this._domain) ||
+                  this.apps.find((app) => {
+                    if (
+                      this._selectedSlug?.slug_id == app.app.app_slug_id &&
+                      app.app.entity_id == entity.entity_id
+                    )
+                      return true;
+                    return false;
+                  })
+                ) {
                   return '';
                 }
                 return entity.entity_id;
@@ -85,12 +108,26 @@ export class AppForm extends LitElement {
           @value-changed=${this.entityChanged}
         ></ha-selector>
 
-        <button type="submit">
+        <button type="submit" class="btn">
           <ha-svg-icon title="submit" .path=${mdiPlus}></ha-svg-icon>
+        </button>
+        <button
+          @click=${(e: any) => {
+            e.preventDefault();
+            this._sortable = !this._sortable;
+          }}
+          class="btn reorder-btn"
+        >
+          <ha-svg-icon title="reorder" .path=${mdiSort}></ha-svg-icon>
         </button>
       </form>
 
-      <sk-reorderable-list .items="${this.appList}"></sk-reorderable-list>
+      <sk-reorderable-list
+        .hass=${this.hass}
+        .appSlugs=${this.appSlugs}
+        .apps="${this.apps}"
+        .sortable=${this._sortable}
+      ></sk-reorderable-list>
     `;
   }
   handleSubmit = (e: Event) => {
@@ -99,30 +136,34 @@ export class AppForm extends LitElement {
     if (!this._selectedSlug || !this._selectedEntity) return; //TODO HANDLE
 
     const appListItem: AppListItem = {
-      app_id: `${this._selectedSlug.slug}-${this._selectedEntity.entity_id}`,
+      app: {
+        app_id: `${this._selectedSlug.slug_id}-${this._selectedEntity.entity_id}`,
+        app_slug_id: this._selectedSlug.slug_id,
+        entity_id: this._selectedEntity.entity_id,
+      },
       app_slug: this._selectedSlug,
       entity: this._selectedEntity,
     };
     if (
-      !this.appList.find(
+      !this.apps.find(
         (appEntity) =>
           appEntity.app_slug == appListItem.app_slug &&
           appEntity.entity == appListItem.entity,
       )
     )
-      this.appList.push(appListItem);
-    this.appList = [...this.appList]; //TODO VALIDATE DATA _entity could be null for example
+      this.apps.push(appListItem);
+    this.apps = [...this.apps]; //TODO VALIDATE DATA _entity could be null for example
 
-    saveApp(this.hass, appListItem); //SAVE TO STORAGE
+    saveApp(this.hass, appListItem.app); //SAVE TO STORAGE
 
     this.requestUpdate();
   };
 
   listApps() {
-    return html`${this.appList.map((app) => {
-      const id = `${app.app_slug.slug}-${app.entity!.entity_id}`;
+    return html`${this.apps.map((app) => {
+      const id = `${app.app_slug.slug_id}-${app.entity!.entity_id}`;
       return html`<li .id="${id}">
-        ${app.app_slug.friendlyName} - ${app.entity!.attributes.friendly_name}
+        ${app.app_slug.friendly_name} - ${app.entity!.attributes.friendly_name}
       </li>`;
     })}`;
   }
@@ -131,7 +172,7 @@ export class AppForm extends LitElement {
     //TODO CREATE TYPE MATCHING EVENT LOOK AT COMMENTED TYPE UP TOP ETC https://github.com/home-assistant/frontend/blob/dev/src/common/dom/fire_event.ts#L60
     //!https://github.com/home-assistant/frontend/blob/dev/src/common/dom/fire_event.ts#L60
     this._selectedSlug =
-      this.appSlugs.find((app) => app.slug == e.detail.value) ?? null;
+      this.appSlugs.find((app) => app.slug_id == e.detail.value) ?? null;
     this._domain = this._selectedSlug?.domain ?? '';
     this._selectedEntity = null;
     this.requestUpdate();
