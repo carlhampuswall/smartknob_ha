6dd6dce994dc434d64aa3c1af4678696d6db1a2c
Hampus Wall <carl.hampus.wall@gmail.com>
2023-12-11 16:27:10 +0000

    mqtt and websockets are now working exposing knobs dict to the frontend

--- custom_components/smartknob/store.py
+++ custom_components/smartknob/store.py
@@ -1,21 +1,15 @@
-from email.policy import default
-from typing import MutableMapping, cast
+from collections import OrderedDict
+from collections.abc import MutableMapping
+from typing import Dict, cast
+
 import attr
 
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.loader import bind_hass
-from .logger import _LOGGER
 from homeassistant.helpers.storage import Store
+from homeassistant.loader import bind_hass
 
-from .const import (
-    DATA_REGISTRY,
-    DOMAIN,
-    LIGHT_DIMMER,
-    LIGHT_SWITCH,
-    SAVE_DELAY,
-    STORAGE_KEY,
-    SWITCH,
-)
+from .const import DATA_REGISTRY, SAVE_DELAY, STORAGE_KEY
+from .logger import _LOGGER
 
 
 @attr.s(slots=True, frozen=True)
@@ -28,29 +22,54 @@ class AppEntry:
     friendly_name = attr.ib(type=str, default=None)
 
 
+@attr.s(slots=True, frozen=True)
+class SmartknobConfig:
+    """Smartknob device configuration, storage entry."""
+
+    mac_address = attr.ib(type=str, default=None)
+    apps = attr.ib(type=list[AppEntry], default=None)
+
+
 class SmartknobStorage:
+    """Class to hold Smartknob storage."""
+
     def __init__(self, hass: HomeAssistant) -> None:
+        """Initialize the Smartknob storage."""
         self.hass = hass
         self.config: MutableMapping[
             str, str
         ] = {}  #! ADD SMARTKNOB DEVICE SPECIFIC CONFIG HERE
-        self.apps = []
+        self.knobs: MutableMapping[str, SmartknobConfig] = {}
         self._store = Store(hass, 1, STORAGE_KEY)
 
     async def async_load(self) -> None:
+        """Load the registry of Smartknob."""
         data = await self._store.async_load()
+        knobs: "OrderedDict[str, AppEntry]" = OrderedDict()
+
         if data is None:
             return
-        if "apps" in data:
-            for app in data["apps"]:
-                self.apps.append(
+
+        if "knobs" in data:
+            for knob in data["knobs"]:
+                apps = [
                     AppEntry(
                         app_id=app["app_id"],
                         app_slug_id=app["app_slug_id"],
                         entity_id=app["entity_id"],
                         friendly_name=app["friendly_name"],
                     )
+                    for (app) in knob["apps"]
+                ]
+                knobs[knob["mac_address"]] = SmartknobConfig(
+                    mac_address=knob["mac_address"], apps=apps
                 )
+
+        self.knobs = knobs
+
+        # TODO ADD CHECK IF NO APPS
+        # if not apps:
+        #     await self.async_factory_default()
 
     @callback
     def async_schedule_save(self) -> None:
@@ -58,13 +77,12 @@ class SmartknobStorage:
         self._store.async_delay_save(self._data_to_save, SAVE_DELAY)
 
     async def async_save(self) -> None:
+        """Save the registry of Smartknob."""
         await self._store.async_save(self._data_to_save())
 
     @callback
     def _data_to_save(self) -> dict:
-        store_data = {
-            "apps": [attr.asdict(app) for app in self.apps],
-        }
+        store_data = {"knobs": [attr.asdict(entry) for entry in self.knobs.values()]}
 
         # EXAMPLE OF ADDING MORE DATA TO STORE
         # store_data["apps"] = [attr.asdict(entry) for entry in self.areas.values()]
@@ -72,80 +90,64 @@ class SmartknobStorage:
         return store_data
 
     async def async_delete(self):
+        """Delete all registry data."""
         _LOGGER.warning("Removing Smartknob configuration data!")
         await self._store.async_remove()
         self.config = {}
-        self.apps = []
+        self.knobs = []
 
     @callback
-    def async_get_app(self, app_id: str):
-        res = None
-        for app in self.apps:
-            if app.app_id == app_id:
-                res = attr.asdict(app)
+    def async_get_knob(self, mac_address: str):
+        """Get smartknob by mac_address."""
+        res = self.knobs.get(mac_address)
+        return attr.asdict(res) if res else None
+
+    @callback
+    def async_get_knobs(self):
+        """Get all smartknobs."""
+        res = {}
+        for key, val in self.knobs.items():
+            res[key] = attr.asdict(val)
         return res
 
     @callback
-    def async_get_apps(self):
-        res = []
-        for app in self.apps:
-            res.append(attr.asdict(app))
-        return res
+    def async_init_knob(self, data: dict) -> SmartknobConfig:
+        """Init new smartknob and add to registry."""
+        new_knob = SmartknobConfig(**data)
+        self.knobs[data["mac_address"]] = new_knob
+        self.async_schedule_save()
+        return attr.asdict(new_knob)
 
     @callback
-    def async_create_app(self, data: dict) -> AppEntry:
+    async def async_add_app(self, mac_address, data: dict) -> AppEntry:
+        """Add new app to registry."""
         new_app = AppEntry(**data)
-        self.apps.append(new_app)
+        self.knobs[mac_address].apps.append(new_app)  #! GOOD WAY TO DO THIS?
         self.async_schedule_save()
         return attr.asdict(new_app)
 
     @callback
-    def async_delete_app(self, app_id: str) -> None:
-        if app_id in self.apps:
-            for app in self.apps:
-                if app.app_id == app_id:
-                    self.apps.remove(app)
+    def async_delete_knob(self, mac_address: str) -> None:
+        """Remove a smartknob from registry."""
+        if mac_address in self.knobs:
+            del self.knobs[mac_address]
             self.async_schedule_save()
             return True
         return False
 
-    # @callback
-    # def async_update_app(self, index, data: dict) -> AppEntry:
-
-    #     # old = None
-    #     # for app in self.apps:
-    #     #     if app.app_id == data.get("app_id"):
-    #     #         old = app
-
-    #     # _LOGGER.error("Old: %s", old)
-    #     # self.apps.remove(old)
-
-    #     # new = self.apps = attr.evolve(old, **data)
-    #     # self.async_schedule_save()
-    #     # return attr.asdict(new)
-
     @callback
-    def async_update_apps(self, new_apps):
+    def async_update_knobs(self, new_knobs):
         """Update existing config."""
-
-        # old = self.apps
-        # new = self.apps = attr.evolve(old, **changes)
-        # self.async_schedule_save()
-        # return attr.asdict(new)
-        old = self.apps
         new = []
 
-        for app in new_apps:
-            new_app = AppEntry(**app)
-            new.append(new_app)
+        for knob in new_knobs:
+            new_knob = SmartknobConfig(**knob)
+            new.append(new_knob)
 
-        self.apps = new
-
+        self.knobs = new
         self.async_schedule_save()
 
         return attr.asdict(new)
-        # _LOGGER.error("OLD: %s", old)
-        # _LOGGER.error("HERE: %s", changes)
 
 
 @bind_hass
